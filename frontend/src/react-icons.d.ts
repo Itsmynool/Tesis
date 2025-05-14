bcbc9ba563854103b2d44d08f14a2e07f4fd8d61
Itsmynool <124412539+Itsmynool@users.noreply.github.com>
2025-05-14 18:52:09 -0500

    Delete frontend directory

--- frontend/src/react-icons.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-icons/fa' {
-    import { ComponentType, SVGProps } from 'react';
-    export const FaWater: ComponentType<SVGProps<SVGSVGElement>>;
-    export const FaCloud: ComponentType<SVGProps<SVGSVGElement>>;
-    export const FaLightbulb: ComponentType<SVGProps<SVGSVGElement>>;
-    export const FaWalking: ComponentType<SVGProps<SVGSVGElement>>;
-    export const FaSmog: ComponentType<SVGProps<SVGSVGElement>>;
-  }
